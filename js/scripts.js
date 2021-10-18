62b45405b6089215c8bb201718e76ea94169b4c3
Matt Luker <jamesmattluker@gmail.com>
2021-10-18 14:48:15 -0600

    add initial  project folders and files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $("h1").click(function() {
+    alert("This is a heading.");
+  });
+
+  $("p").click(function() {
+    alert("THis is a paragraph");
+  });
+
+  $("img").click(function() {
+    alert("This is an image.");
+  });
+});
